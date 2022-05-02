ef767063ec21524ed3bfa5abee34d7e5e8d7fb64
Anzo52 <azollner2014@gmail.com>
2022-05-02 14:24:48 -0600

    Update Intro.java

--- Book1/Intro.java
+++ Book1/Intro.java
@@ -1,5 +1,18 @@
+/* Sing, O goddess, the anger of Achilles son of Peleus, that brought countless ills upon the Achaeans. 
+Many a brave soul did it send hurrying down to Hades, and many a hero did it yield a prey to dogs and vultures, 
+for so were the counsels of Jove fulfilled from the day on which the son of Atreus, 
+king of men, and great Achilles, first fell out with one another.
+*/
 public class Intro {
-  public static void muse() {
-    String sing = "Sing!"
-  }
+  Pantheon.goddess.sing()
+  // Achilles (son of Peleus)
+  // countlessIlls (get/set)
+  // soul
+  // Hades
+  // hero
+  // dogsAndVultures
+  // Jove
+  // Hector (son of Atreus)
+  // king
+  // men
 }
